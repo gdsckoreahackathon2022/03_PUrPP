537c5082ce84a9ad3bcdb379ca89b51bb9642fc0
y00njae <rladbswo12@gist.ac.kr>
2022-02-05 10:04:50 +0900

    feat(main.js): 마무리 game over 까지 구현

--- client/main.js
+++ client/main.js
@@ -17,6 +17,18 @@ ice394.src = 'ice394.png';
 var ice900 = new Image();
 ice900.src = 'ice900.png';
 
+var fish1 = new Image();
+fish1.src = 'fish1.png';
+var fish2 = new Image();
+fish2.src = 'fish2.png';
+var fish3 = new Image();
+fish3.src = 'fish3.png';
+var fish4 = new Image();
+fish4.src = 'fish4.png';
+
+var gameover = new Image();
+gameover.src = 'gameover.png';
+
 var dinox = 10;
 var dinoy = base2;
 
@@ -46,7 +58,7 @@ var sea = {
   width: canvas.width,
   height: canvas.height - base2,
   draw() {
-    ctx.fillStyle = 'blue';
+    ctx.fillStyle = `rgb(0,128,255)`;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   },
 };
@@ -79,6 +91,71 @@ class Cactus900 {
   }
 }
 
+class Fish1 {
+  constructor() {
+    this.x = 580;
+    this.y = 400;
+    this.width = 80;
+    this.height = 80;
+  }
+  draw() {
+    // ctx.fillStyle = 'black';
+    // ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(fish1, this.x, this.y);
+  }
+}
+
+class Fish2 {
+  constructor() {
+    this.x = 650;
+    this.y = 500;
+    this.width = 150;
+    this.height = 150;
+  }
+  draw() {
+    // ctx.fillStyle = 'black';
+    // ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(fish2, this.x, this.y);
+  }
+}
+
+class Fish3 {
+  constructor() {
+    this.x = 730;
+    this.y = 400;
+    this.width = 150;
+    this.height = 150;
+  }
+  draw() {
+    // ctx.fillStyle = 'black';
+    // ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(fish3, this.x, this.y);
+  }
+}
+
+class Fish4 {
+  constructor() {
+    this.x = 800;
+    this.y = 500;
+    this.width = 150;
+    this.height = 150;
+  }
+  draw() {
+    ctx.drawImage(fish4, this.x, this.y);
+  }
+}
+
+var endGame = {
+  x: 100,
+  y: 100,
+  width: 400,
+  height: 100,
+  state: true,
+  draw() {
+    ctx.drawImage(gameover, this.x, this.y);
+  },
+};
+
 const fps = 25;
 var animation;
 var a = 2;
@@ -87,6 +164,10 @@ var timer = 0;
 var jumpTimer = 0;
 var isJumping = false;
 var cactusArray = [];
+var fishArray = [];
+var fishDeltaY1 = 1;
+var fishDeltaY2 = -1;
+var starting = false;
 
 var cactusTemp1 = new Cactus900();
 cactusTemp1.x = 0;
@@ -98,6 +179,15 @@ var cactusTemp3 = new Cactus394();
 cactusTemp3.x = 1400;
 cactusArray.push(cactusTemp3);
 
+var fishTemp1 = new Fish1();
+fishArray.push(fishTemp1);
+var fishTemp2 = new Fish2();
+fishArray.push(fishTemp2);
+var fishTemp3 = new Fish3();
+fishArray.push(fishTemp3);
+var fishTemp4 = new Fish4();
+fishArray.push(fishTemp4);
+
 function draw() {
   // setTimeout(() => {
   //   animation = requestAnimationFrame(draw);
@@ -107,9 +197,16 @@ function draw() {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (timer % 80 === 0) {
-    var cactus = new Cactus394();
-    cactusArray.push(cactus);
+  if (timer < 60 * 5) {
+    if (timer % 80 === 0) {
+      var cactus = new Cactus394();
+      cactusArray.push(cactus);
+    }
+  } else {
+    if (timer % 95 === 0) {
+      var cactus = new Cactus394();
+      cactusArray.push(cactus);
+    }
   }
 
   if (isJumping == true) {
@@ -158,11 +255,26 @@ function draw() {
       dinoy = base2;
     }
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    endGame.draw();
     cancelAnimationFrame(animation);
   }
 
   dino.draw();
   sea.draw();
+
+  fishArray.forEach((element, index, array) => {
+    element.draw();
+    if (index % 2 == 0) {
+      element.y += fishDeltaY1;
+    } else {
+      element.y += fishDeltaY2;
+    }
+  });
+
+  if (timer % 20 === 0) {
+    fishDeltaY1 = -fishDeltaY1;
+    fishDeltaY2 = -fishDeltaY2;
+  }
 
   cactusArray.forEach((element, index, array) => {
     element.x -= 7;
@@ -178,6 +290,9 @@ function draw() {
 draw();
 
 document.addEventListener('keydown', (event) => {
+  if (!starting) {
+    starting = true;
+  }
   if (event.code == 'Space') {
     v0 = -30;
     isJumping = true;
@@ -189,4 +304,10 @@ function isCollision(a, b) {
   var diffy = b.y - (a.y + a.height);
   if (diffx <= 0 && diffy <= 0) return true;
   else return false;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
 }
